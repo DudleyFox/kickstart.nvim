2c27168b5f1a00a36544889ba864b8cf1343bedb
fox <dudley.fox@gmail.com>
2024-12-15 14:12:29 -0600

    Add toggle for neotree

--- init.lua
+++ init.lua
@@ -163,6 +163,8 @@ vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<C-\\>', '<cmd>Neotree toggle<CR>')
+vim.keymap.set('i', '<C-\\>', '<cmd>Neotree toggle<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
